f4d417a4a751e930b363cce551c528a7e792a6ff
Jiazhou Xiao <37034774+jiazhouxiao@users.noreply.github.com>
2018-06-10 15:53:25 -0700

    Scaffolding TX struct

--- client/src/structs/tx.go
+++ client/src/structs/tx.go
@@ -1,5 +1,16 @@
 package structs
 
+type TXOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+type TXInput struct {
+	Txid      []byte
+	Vout      int
+	ScriptSig string
+}
+
 type Transaction struct {
 	ID   []byte
 	Vin  []TXInput
