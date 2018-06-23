a4ee718f1fb490bc5f853b818a38e409ed1102c5
Jiazhou Xiao <jiazhouxiao@jiazhouxiao-macbookpro2.roam.corp.google.com>
2018-06-23 16:06:00 -0700

    add preliminary version of transaction struct

---
+++ client/src/structs/tx.go
@@ -0,0 +1,18 @@
+package structs
+
+type Transaction struct {
+	ID   []byte
+	Vin  []TXInput
+	Vout []TXOutput
+}
+
+type TXOutput struct {
+	Value        int
+	ScriptPubKey string
+}
+
+type TXInput struct {
+	Txid      []byte
+	Vout      int
+	ScriptSig string
+}
